54ddb1e613771028c3818dabb7f2ef087dd6c2d2
Emoumo-coder <sandaumaru207@gmail.com>
2024-05-16 17:35:28 +0100

    Lesson 22 completed

--- JAVASCRIPT/LESSON 22/js/script.js
+++ JAVASCRIPT/LESSON 22/js/script.js
@@ -55,4 +55,88 @@ console.log("This is the mud of 4 and 2: ", j);
 console.log("\n Increment>>>");
 
 x += 3;
+x += 3;
+// x++;
 console.log("This is the increment of x and 3: ", x);
+
+console.log("\n Decrement>>>");
+
+x -= 3;
+x -= 3;
+// x++;
+console.log("This is the Decrement of x and 3: ", x);
+
+console.log("\n Multiplication short syntax>>>");
+
+// y = y * 2;
+y *= 2;
+console.log("This is the multi of y and 2: ", y); // same also goes for divison , multiplication modulus, and exponentiation.
+
+
+/*============== COMPARISON OPERATORS ===============*/
+let comA = 5;
+let comB = '5';
+// let comResult = comA == comB; /* insensitive equal comparison */
+let comResult = comA === comB;
+
+console.log(comResult);
+console.log(typeof comB);
+
+// negative equal comparison
+let comC = '10';
+let comD = '10';
+// let comResultB = comC != comD; /* insensitive not equal comparison */
+let comResultB = comC !== comD;
+
+// Greater than sign
+let comE = 5;
+let comF = 8;
+
+let comResultC = comE > comF;
+
+console.log(comResultC);
+
+// Greater than sign
+let comG = 5;
+let comH = 8;
+
+let comResultD = comE < comF;
+
+// greater than or equal operator
+
+console.log(comResultD);
+
+// Greater  than or  sign
+let comI = 5;
+let comJ = 8;
+
+let comResultE = comI >= comJ;
+
+console.log(comResultE);
+
+// Logical operators
+let opA = 5;
+let opB = 25;
+
+let opResult = 2 > 1 && 1 == 1;
+//  Or logical operator
+let opResultB = 2 > 1 || 1 == 2;
+
+// 5 + 3
+
+console.log(!opResult);
+
+/*============== Concatenation on string ===============*/
+
+console.log("\n\n\n\n\n\n\n\n\n\n");
+
+
+//  String concatenation
+let str = "I am ";
+let myName = "Umar";
+const bootcamp = "Hint Media";
+let str2 = "I am from the " + bootcamp + " Bootcamp"
+
+let completeMessage = str + myName + "\n\n\n\n" + str2;
+
+console.log(completeMessage);
