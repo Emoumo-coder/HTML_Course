bcd375781c69997d4fe0030e28926f728bedadd7
Emoumo-coder <sandaumaru207@gmail.com>
2024-05-16 15:33:17 +0100

    Lesson 22 undone

---
+++ JAVASCRIPT/LESSON 22/README.md
@@ -0,0 +1,45 @@
+# "Lesson 22 Objective"
+
+## Topics
+
+- Operators
+
+### String
+
+- length property
+- charAt method
+- indexOf method
+- lastIndexOf method
+- slice method
+- toUpperCase method
+- toLowerCase method
+- includes method
+- split method
+
+### Number
+
+- Comparison operators
+- Number function
+- isInteger method on Number
+- parseFloat method on Number
+- parseInt method on Number
+- toFixed method on Number
+- isNaN method on Number
+- toString method
+- NaN Global Function
+
+### Math
+
+- Math.PI property
+- Math.trunc method
+- Math.round method
+- Math.ceil method
+- Math.floor method
+- Math.pow method
+- Math.max method
+- Math.min method
+- Math.random method
+
+### Excercise
+
+- Generate a random character from anyone's name.
---
+++ JAVASCRIPT/LESSON 22/js/script.js
@@ -0,0 +1,58 @@
+let x = 22;
+let y = 12;
+
+
+console.log("THis is the value of x and y", x, y);
+
+// operators
+let z = x + 7;
+let z1 = x + y;
+console.log("This is the sum of x and 7", z);
+console.log("This is the sum of x and y", z1);
+
+console.log("\nSubtraction>>>");
+
+let a = x - 7;
+let b = x - y;
+console.log("This is the sub of x and 7", a);
+console.log("This is the sub of x and y", b);
+
+console.log("\nMultiplication>>>");
+
+let c = x * 7;
+let d = x * y;
+console.log("This is the mul of x and 7", c);
+console.log("This is the mul of x and y", d);
+
+
+console.log("\nDivision>>>");
+
+let e = x / 7;
+let f = x / y;
+console.log("This is the div of x and 7", e);
+console.log("This is the div of x and y", f);
+
+console.log("\n Equation>>>");
+
+// s = a + (b - c)
+// let say: a = 2; b = 3; c = 4;
+let s = 2 + (3 - 4);
+console.log("This is result of s = a + (b - c) expression", s);
+
+
+console.log("\n Exponentiation>>>");
+
+let i = 2 ** 4;
+console.log("This is the power of 2 and 2: ", i);
+
+
+console.log("\n Modulus>>>");
+
+let j = 7 % 2;
+console.log("This is the mud of 4 and 2: ", j);
+
+
+console.log("\n Increment>>>");
+
+x += 3;
+console.log("This is the increment of x and 3: ", x);
