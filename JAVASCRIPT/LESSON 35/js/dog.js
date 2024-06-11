79ab150c0fdce6141d880093a9089b5160e1e1d5
Emoumo-coder <sandaumaru207@gmail.com>
2024-06-11 16:58:29 +0100

    lesson 35 done

--- JAVASCRIPT/LESSON 35/js/dog.js
+++ JAVASCRIPT/LESSON 35/js/dog.js
@@ -1,46 +1,46 @@
 export function dog () {
     let dogHasEaten = confirm("Has dog 🐕 eaten ?");
 
-const dogEat = () => {
-  return new Promise((resolve, reject) => {
-    dogHasEaten
-      ? resolve("The dog has eaten Bread 🍞")
-      : reject("The dog hasn't eaten");
-  });
-};
+    const dogEat = () => {
+    return new Promise((resolve, reject) => {
+        dogHasEaten
+        ? resolve("The dog has eaten Bread 🍞")
+        : reject("The dog hasn't eaten");
+    });
+    };
 
-const walkDog = () => {
-  let haveWalkedDog = confirm("Have you taken the dog 🐕 to walk 🚶‍♀️ ?");
+    const walkDog = () => {
+    let haveWalkedDog = confirm("Have you taken the dog 🐕 to walk 🚶‍♀️ ?");
 
-  return new Promise((resolve, reject) => {
-    haveWalkedDog
-      ? resolve("I have walked the dog 🐕 through a long street 🚥")
-      : reject("It hasn't been taken to walk 🚶‍♀️");
-  });
-};
+    return new Promise((resolve, reject) => {
+        haveWalkedDog
+        ? resolve("I have walked the dog 🐕 through a long street 🚥")
+        : reject("It hasn't been taken to walk 🚶‍♀️");
+    });
+    };
 
-dogEat()
-  .then((data) => {
-    console.log(data);
-    return 4;
-  })
-  .then((data) => {
-    console.log(data);
-    return "The dog hasn't barked yet.";
-  })
-  .then((data) => {
-    console.log(data);
-    walkDog()
-      .then((data) => {
+    dogEat()
+    .then((data) => {
         console.log(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+        return 4;
+    })
+    .then((data) => {
+        console.log(data);
+        return "The dog hasn't barked yet.";
+    })
+    .then((data) => {
+        console.log(data);
+        walkDog()
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    })
+    .catch((error) => {
+        console.error(error);
+    });
 
-console.log("This is running an app concerning dog chores");
+    console.log("This is running an app concerning dog chores");
 }
--- JAVASCRIPT/LESSON 35/js/script.js
+++ JAVASCRIPT/LESSON 35/js/script.js
@@ -2,4 +2,45 @@
 
 import { dog } from "./dog.js";
 
-// dog();
+// dog()
+
+// /*============== Fetch API ===============*/
+console.log("\n\n\n\n\n\nFetch API");
+
+const usersData = {
+    userEmails: [],
+    userWebsites: []
+};
+
+const users = fetch("https://jsonplaceholder.typicode.com/users");
+
+users.then((response) => {
+    if (response.ok) {
+        console.log(response);
+        return response.json();
+    } else {
+        console.log("Your request went wrong!");
+    }
+}).then((users) => {
+    // first task
+    users.map((user) => {
+        usersData.userEmails.push(user.email);
+        usersData.userWebsites.push(user.website);
+    });
+    console.log(usersData);
+
+    // second task: dump the data to the html table.
+    dumpTotable(users);
+
+})
+
+console.log("Working💪💪...");
+
+console.log(usersData);
+
+function dumpTotable (users) {
+
+    const tableBody = document.querySelector('tbody');
+
+    tableBody.innerHTML = users;
+}
