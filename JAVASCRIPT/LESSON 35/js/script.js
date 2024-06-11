1fefe7f892442c076b6b52af33e8fe1c8fbd1503
Emoumo-coder <sandaumaru207@gmail.com>
2024-06-11 14:44:47 +0100

    Lesson 35 dog completed

--- JAVASCRIPT/LESSON 35/js/script.js
+++ JAVASCRIPT/LESSON 35/js/script.js
@@ -1,24 +1,5 @@
 // /*============== Promises ===============*/
-let dogHasEaten = confirm("Has dog 🐕 eaten ?");
 
-const dogEat = () => {
-  return new Promise((resolve, reject) => {
-    dogHasEaten
-      ? resolve("The dog has eaten Bread 🍞")
-      : reject("The dog hasn't eaten");
-  });
-};
+import { dog } from "./dog.js";
 
-dogEat().then((data) => {
-    console.log(data);
-    return 4;
-})
-.then((data) => {
-    console.log(data);
-    return "The dog hasn't barked yet."
-}).then((data) => {
-    console.log(data);
-})
-.catch((error) => {
-    console.error(error);
-});
+// dog();
